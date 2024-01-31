a6760d7c273641fc1d0bbe9918cef85c75222aa0
TreeMarie <146091715+TreeMarie@users.noreply.github.com>
2024-01-31 09:11:47 -0500

    Update script.js

--- Coursera Test/Module 4 Solution/js/script.js
+++ Coursera Test/Module 4 Solution/js/script.js
@@ -1,8 +1,17 @@
-(function () {
-
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names =new Array(); 
+names[0]="Yaakov"
+names[1]="John"
+names[2]="Jen"
+names[3]="Jason"
+names[4]="Paul"
+names[5]="Frank"
+names[6]="Larry"
+names[7]="Paula"
+names[8]="Laura"
+names[9]="Jim"
     
     for (var i = 0; i < names.length; i++) {
+        console.log(names[i]) } 
     
       var firstLetter = names[i].charAt(0).toLowerCase();
       if (firstLetter === 'j') {
