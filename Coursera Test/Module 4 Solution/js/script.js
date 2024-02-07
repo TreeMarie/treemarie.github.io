44dfe911f852e365855cdc0746b8f8df2e355bd4
TreeMarie <146091715+TreeMarie@users.noreply.github.com>
2024-02-07 15:19:56 -0500

    Update script.js

--- Coursera Test/Module 4 Solution/js/script.js
+++ Coursera Test/Module 4 Solution/js/script.js
@@ -1,19 +1,19 @@
-var names =new Array(); 
-names[0]="Yaakov"
-names[1]="John"
-names[2]="Jen"
-names[3]="jason"
-names[4]="paul"
-names[5]="Frank"
-names[6]="larry"
-names[7]="paula"
-names[8]="Laura"
-names[9]="jim"
-    console.log(names[0].chartAt(0));
-    for (var i = 0; i < names.length; i++) {
-        if(names[i].chartAt(0)==='J' (names[i].chartAt(0)==='j'){
+var names=new Array(); 
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="Frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="Laura";
+names[9]="jim";
+    
+    for (var i = 0; i <names.length; i++) {
+        if(names[i].chartAt(0)==='J' then (names[i].chartAt(0)==='j'){
            console.log("Goodbye "+ names[i]) }
         else {console.log("Hello "+ names[i]) }
             
     
-     
+     console.log(names[0].chartAt(0));
