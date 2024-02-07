babe72af915cc28282c1eca5315dc16f13c26d23
TreeMarie <146091715+TreeMarie@users.noreply.github.com>
2024-02-07 15:22:58 -0500

    Update script.js

--- Coursera Test/Module 4 Solution/js/script.js
+++ Coursera Test/Module 4 Solution/js/script.js
@@ -13,7 +13,9 @@ names[9]="jim";
     for (var i = 0; i <names.length; i++) {
         if(names[i].chartAt(0)==='J' || (names[i].chartAt(0)==='j'){
            console.log("Goodbye "+ names[i]) }
-        else {console.log("Hello "+ names[i]) }
+        else {
+            console.log("Hello "+ names[i]) 
+        }
             
     
-     console.log(names[0].chartAt(0));
+    
