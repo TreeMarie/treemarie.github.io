1954bd7e906715b5018fce8a97855061d8f07f77
TreeMarie <146091715+TreeMarie@users.noreply.github.com>
2024-02-07 16:51:21 -0500

    Update SpeakHello.js

--- Coursera Test/Module 4 Solution/js/SpeakHello.js
+++ Coursera Test/Module 4 Solution/js/SpeakHello.js
@@ -1,10 +1,10 @@
 
 (function (window) {  
     var helloSpeaker = {};
-    var speakWord = "Hello";
+    var speakWord = "Hello ";
     
     helloSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
+      console.log(speakWord + names);
     }
         window.helloSpeaker = helloSpeaker;
     })(window);
