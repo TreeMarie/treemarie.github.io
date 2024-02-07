ea81a7b238acdfa5dd8f1f178d9b1f3eafecd15c
TreeMarie <146091715+TreeMarie@users.noreply.github.com>
2024-02-07 15:04:53 -0500

    Update script.js

--- Coursera Test/Module 4 Solution/js/script.js
+++ Coursera Test/Module 4 Solution/js/script.js
@@ -12,16 +12,8 @@ names[9]="jim"
     console.log(names[0].chartAt(0));
     for (var i = 0; i < names.length; i++) {
         if(names[i].chartAt(0)==='J' (names[i].chartAt(0)==='j'){
-           // console.log("Goodbye "+ names[i]) // }
+           console.log("Goodbye "+ names[i]) }
+        else {console.log("Hello "+ names[i]) }
             
     
-      var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } 
-      else {
-        helloSpeaker.speak(names[i]);
-      }
-    }
-    
-    })();
+     
